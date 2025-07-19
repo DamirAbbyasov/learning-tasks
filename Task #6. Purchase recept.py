70a4ce06ea06ab5e9f14812637107ffc9dcebc92
Damir <damirwvw@gmail.com>
2025-07-19 19:11:17 +0300

    Delete Task #6. Purchase recept.py

--- Task #6. Purchase recept.py
+++
@@ -1,7 +0,0 @@
-name = input()
-price = int(input())
-weight = int(input())
-buyer_has = int(input())
-cost = price * weight
-change = buyer_has - cost
-print(f"Чек\n{name} - {weight}кг - {price}руб/кг\nИтого: {cost}руб\nВнесено: {buyer_has}руб\nСдача: {change}руб")
